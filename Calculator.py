9b1127cb0eb57842454831071b260c307ec5f92d
bharadjaya-collab <bharadjaya@gmail.com>
2025-10-24 22:22:55 +0530

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,43 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return "Error: Division by Zero"
+    return x / y
+
+
+print("=== Simple Calculator===")
+print(" Choose operator:")
+print("1.Add (+)")
+print("2.Subtract(-)")
+print("3.Multiply (*)")
+print("4.Divide (/)")
+
+op = input("Enter your choice (+,-,*,/):")
+a = float(input("Enter first number:"))
+b = float(input("Enter second number:"))
+
+if op == '+':
+    result = add(a, b)
+elif op == '-':
+    result = Subtract(a, b)
+elif op == '*':
+    result = Multiply(a, b)
+elif op == '/':
+    result = divide(a, b)
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
+
+
